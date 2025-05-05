1bac9a990044e26455fc8a1d2fdea22cddda9481
blakejiang <2446355540@qq.com>
2025-05-05 18:01:36 +0800

    feat: 修改数据格式

--- entity/ft/ft_pool_history.go
+++ entity/ft/ft_pool_history.go
@@ -7,8 +7,8 @@ import (
 // TBC20PoolHistoryRequest 获取池子历史记录请求
 type TBC20PoolHistoryRequest struct {
 	PoolId string `uri:"pool_id" binding:"required"` // 池子ID
-	Page   int    `uri:"page"`                      // 页码，从0开始
-	Size   int    `uri:"size"`                      // 每页大小
+	Page   int    `uri:"page"`                       // 页码，从0开始
+	Size   int    `uri:"size"`                       // 每页大小
 }
 
 // Validate 验证请求参数的合法性
@@ -36,13 +36,13 @@ type TBC20PoolHistoryResponse struct {
 	Txid                        string `json:"txid"`                             // 交易ID
 	PoolId                      string `json:"pool_id"`                          // 池子ID
 	ExchangeAddress             string `json:"exchange_address"`                 // 交换地址
-	FtLpBalanceChange           string `json:"ft_lp_balance_change"`             // LP代币余额变化
+	FtLpBalanceChange           *int64 `json:"ft_lp_balance_change"`             // LP代币余额变化
 	TokenPairAId                string `json:"token_pair_a_id"`                  // 代币A的ID（通常是"TBC"）
 	TokenPairAName              string `json:"token_pair_a_name"`                // 代币A的名称（通常是"TBC"）
 	TokenPairADecimal           int    `json:"token_pair_a_decimal"`             // 代币A的小数位数
-	TokenPairAPoolBalanceChange string `json:"token_pair_a_pool_balance_change"` // 代币A的池子余额变化
+	TokenPairAPoolBalanceChange *int64 `json:"token_pair_a_pool_balance_change"` // 代币A的池子余额变化
 	TokenPairBId                string `json:"token_pair_b_id"`                  // 代币B的ID
 	TokenPairBName              string `json:"token_pair_b_name"`                // 代币B的名称
 	TokenPairBDecimal           int    `json:"token_pair_b_decimal"`             // 代币B的小数位数
-	TokenPairBPoolBalanceChange string `json:"token_pair_b_pool_balance_change"` // 代币B的池子余额变化
+	TokenPairBPoolBalanceChange *int64 `json:"token_pair_b_pool_balance_change"` // 代币B的池子余额变化
 }
